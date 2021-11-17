fe80fe1de3ff59262cc78c77b9345dbea6588582
Freewind <nowindlee@gmail.com>
2021-11-17 19:36:42 +0800

    fix

--- src-client/Hello.tsx
+++ src-client/Hello.tsx
@@ -4,21 +4,23 @@ import axios from 'axios';
 type Props = {};
 
 export const Hello: FC<Props> = ({}) => {
-  const [message, setMessage] = useState("")
+  const [getMessage, setGetMessage] = useState("")
+  const [deleteMessage, setDeleteMessage] = useState("");
 
   async function fetchRemoteMessage() {
     const response = await axios.get<string>('http://localhost:8080/')
-    setMessage(response.data);
+    setGetMessage(response.data);
   }
 
   async function deleteRemoteMessage() {
-    await axios.delete<string>('http://localhost:8080/')
+    const response = await axios.delete<string>('http://localhost:8080/')
+    setDeleteMessage(response.data);
   }
 
   return <div className={'Hello'}>
     <h1>Hello React</h1>
-    <button onClick={() => fetchRemoteMessage()}>Fetch remote message {message}</button>
+    <button onClick={() => fetchRemoteMessage()}>Fetch remote message {getMessage}</button>
     <hr/>
-    <button onClick={() => deleteRemoteMessage()}>Delete remote message</button>
+    <button onClick={() => deleteRemoteMessage()}>Delete remote message {deleteMessage}</button>
   </div>;
 }
--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,25 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                System.out.println("### addCorsMappings");
+                registry.addMapping("/**").allowedOriginPatterns("*");
+            }
+        };
+    }
 }
